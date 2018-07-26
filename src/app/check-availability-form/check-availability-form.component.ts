fbaeded05456b10dbc75edd694bbfdc6a0b3002d
Sem Flamez <Sem.Flamez@smals.be>
2018-07-26 17:20:12 +0200

    Try polling (WIP).

--- src/app/check-availability-form/check-availability-form.component.ts
+++ src/app/check-availability-form/check-availability-form.component.ts
@@ -1,6 +1,9 @@
+import { Observable, of } from 'rxjs';
+import { ResengoService } from './../resengo/resengo.service';
 import { Availability } from '../availability.model';
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { startWith, switchMap, catchError, retry, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-check-availability-form',
@@ -12,9 +15,9 @@ export class CheckAvailabilityFormComponent {
   time = new FormControl();
   nbrOfPeople = new FormControl();
 
-  @Output() availabilityCheck = new EventEmitter<Availability>();
+  @Output() availability = new EventEmitter<Availability>();
 
-  constructor() {
+  constructor(private resengo: ResengoService) {
     const today = new Date();
     const month = today.getMonth() < 9 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1;
     const day = today.getDate() < 10 ? '0' + today.getDate() : today.getDate();
@@ -24,10 +27,30 @@ export class CheckAvailabilityFormComponent {
   }
 
   checkAvailability() {
-    this.availabilityCheck.emit({
+    const availabilityCheck = this.resengo.getAvailability({
       date: this.date.value,
       time: this.time.value,
       nbrOfPeople: this.nbrOfPeople.value
+    }).pipe(
+      retry(),
+      map(this.handleAvailability),
+      catchError(err => of([]))
+    );
+    availabilityCheck.subscribe({
+      next(x) { console.log('data: ' + x); },
+      error(err) { console.error('errors already caught... will no longer run'); }
     });
+  }
+
+  private handleAvailability(availabilities: Availability[]) {
+    console.log(availabilities);
+    if (!availabilities.length) {
+      throw new Error('no availabilities');
+    }
+    if (availabilities.length === 1 && availabilities[0].availabilityStatus === Status.NOT_YET_AVAILABLE) {
+      console.warn('Not YET Available, retrying...');
+      throw new Error('no availabilities');
+    }
+    return availabilities;
   }
 }
